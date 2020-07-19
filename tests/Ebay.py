58c62f46322d6fd5a5004c8359d755046621f79e
Sebastian Leal <sebastianleaj10@gmail.com>
2020-07-19 14:06:43 -0500

    Step2

--- tests/Ebay.py
+++ tests/Ebay.py
@@ -1,7 +1,15 @@
 import unittest
+from selenium import webdriver
 
 
-class MyTestCase(unittest.TestCase):
+class Ebay(unittest.TestCase):
+
+    def setUp(self):
+        # create a new Firefox session
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https://www.ebay.com/")
+
     def test_something(self):
         self.assertTrue(True)
 
