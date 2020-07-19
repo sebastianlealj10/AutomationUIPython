610effeb9f1ab945f6a96237968c7d816a47022e
Sebastian Leal <sebastianleaj10@gmail.com>
2020-07-19 14:17:35 -0500

    changes

--- tests/Ebay.py
+++ tests/Ebay.py
@@ -6,7 +6,7 @@ class Ebay(unittest.TestCase):
 
     def setUp(self):
         # create a new Firefox session
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox(service_log_path='./logs/geckodriver.log')
         self.driver.implicitly_wait(30)
         self.driver.get("https://www.ebay.com/")
 
