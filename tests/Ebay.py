233a5f3f34d4d83e0e79e9a36950a77729186a41
Sebastian Leal <sebastianleaj10@gmail.com>
2020-07-19 19:07:25 -0500

    step6

--- tests/Ebay.py
+++ tests/Ebay.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 from selenium import webdriver
 from homepage import SearchTab
+from searchpage import SortItems
 
 
 class Ebay(unittest.TestCase):
@@ -11,9 +13,16 @@ class Ebay(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.driver.get("https://www.ebay.com/")
 
-    def test_home_page_loaded(self):
+    def test_search_shoes(self):
         home_page = SearchTab(self.driver)
         self.assertTrue(home_page.check_page_loaded())
+        home_page.search_shoes()
+        search_page = SortItems(self.driver)
+        search_page.pick_ten_size_puma()
+        results = search_page.results_number()
+        time.sleep(2)
+        print("Total resuts for the search: " + str(results))
+        print("........................................................................................\n")
 
 
 if __name__ == '__main__':
