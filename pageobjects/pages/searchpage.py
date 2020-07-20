e0165055adabe249467413b2ff5623c46c324e28
Sebastian Leal <sebastianleaj10@gmail.com>
2020-07-19 21:37:05 -0500

    basepage

--- pageobjects/pages/searchpage.py
+++ pageobjects/pages/searchpage.py
@@ -1,22 +1,15 @@
 import time
 
 from selenium.webdriver import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+from BasePage import BasePage
 from items import build_items_list
 from locators import SearchPageLocators
 
 
-class BasePage(object):
-    """Base class to initialize the base page that will be called from all pages"""
-
-    def __init__(self, driver):
-        self.driver = driver
-
-
 class SortItems(BasePage):
-
-    def check_page_loaded(self):
-        return True if self.driver.find_element(*SearchPageLocators.PRICE_NAV) else False
 
     def check_size(self):
         element = self.driver.find_element(*SearchPageLocators.SELECT_SIZE_TEN)
@@ -35,20 +28,20 @@ class SortItems(BasePage):
         return element.text
 
     def sort_list(self):
-        element = self.driver.find_element(*SearchPageLocators.SORT_BUTTON)
+        element = WebDriverWait(self.driver, 10). \
+            until(EC.presence_of_element_located(SearchPageLocators.SORT_BUTTON))
         hover = ActionChains(self.driver).move_to_element(element)
         hover.click().perform()
 
     def pick_lowest_first(self):
-        element = self.driver.find_element(*SearchPageLocators.CHECK_LOWEST_FIRST)
+        element = WebDriverWait(self.driver, 10). \
+            until(EC.presence_of_element_located(SearchPageLocators.CHECK_LOWEST_FIRST))
         hover = ActionChains(self.driver).move_to_element(element)
         hover.click().perform()
 
     def sort_items(self):
-        time.sleep(5)
         self.sort_list()
         self.pick_lowest_first()
-        time.sleep(2)
 
     def pick_element_names(self):
         names = self.driver.find_elements(*SearchPageLocators.ITEMS_NAME)
@@ -59,6 +52,7 @@ class SortItems(BasePage):
         return prices
 
     def take_products(self, number_of_items):
+        time.sleep(2)
         element_names = self.pick_element_names()
         element_prices = self.pick_element_prices()
         my_items = build_items_list(element_names, element_prices, number_of_items)
